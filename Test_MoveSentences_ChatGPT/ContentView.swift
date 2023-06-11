8cd4d1344216f8b4d8fd90bbe51f9b631f784af5
Tom Flahive <TomPFlahive@gmail.com>
2023-06-11 11:22:23 -0700

    Initial Commit

---
+++ Test_MoveSentences_ChatGPT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test_MoveSentences_ChatGPT
+//
+//  Created by Tom Flahive on 6/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test_MoveSentences_ChatGPT/Test_MoveSentences_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_MoveSentences_ChatGPTApp.swift
+//  Test_MoveSentences_ChatGPT
+//
+//  Created by Tom Flahive on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_MoveSentences_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
